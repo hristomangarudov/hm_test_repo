11ac877942cc47432532d53cac3eb170d18c1a0d
hristomangarudov <115619314+hristomangarudov@users.noreply.github.com>
2025-03-04 06:20:07 +0200

    Update HM Script.js

--- HM Script.js
+++ HM Script.js
@@ -1,3 +1,18 @@
+// ==UserScript==
+// @name         HM Script2
+// @namespace    http://tampermonkey.net/HM-script2
+// @version      1.2
+// @description  script made for filtering unique user IP matches also has minigames calculator. Shift + 0 is IP command, while Shift + 9 is for the minigames calc
+// @author       Hristo Mangarudov
+// @match        https://bo2.inplaynet.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=upgaming.com
+// @grant        GM_addStyle
+// @grant        unsafeWindow
+// @grant        window.close
+// @downloadURL  https://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@raw.githubusercontent.com/hristomangarudov/hm_test_repo/main/HM%20Script
+// @updateURL    https://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@raw.githubusercontent.com/hristomangarudov/hm_test_repo/refs/heads/main/HM%20Script.meta.js
+// ==/UserScript==
+
 (function() {
     'use strict';
     /* global $ */
