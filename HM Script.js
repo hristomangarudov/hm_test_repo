b8df76ed2deab2501aec9b09e347b737310a9f1d
hristomangarudov <115619314+hristomangarudov@users.noreply.github.com>
2025-05-18 02:33:34 +0300

    Update HM Script.js
    ts

--- HM Script.js
+++ HM Script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HM Script2
 // @namespace    http://tampermonkey.net/HM-script2
-// @version      3.2.2
+// @version      3.2.3
 // @description  script made for filtering unique user IP matches also has minigames calculator. Shift + 0 is IP command, while Shift + 9 is for the minigames calc
 // @author       Hristo Mangarudov
 // @match        https://bo2.inplaynet.com/*
@@ -127,6 +127,9 @@
     };
   }
   async function fetchTransactionsForCurrentShift(admin = "FraudHM") {
+     if(admin != "FraudHM"){
+         return
+     }
     const { from, to, shiftId } = getCurrentShiftRangeEncoded();
     const lastShownShift = localStorage.getItem("lastShift250Triggered");
 
